3b542852ec411e52aaf93f9fbd833a7db6c2955c
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-02-04 21:34:04 +0900

    Basic CSS

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+const blurb = document.createElement('span');
+const result = document.querySelector('.result');
 
 let humanScore = 0;
 let computerScore = 0;
 let numberOfGames = 0;
-
-function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) return "rock";
-    if (randomNumber === 1) return "paper";
-    else return "scissors"
-}
 
 function getHumanChoice() {
     while (true) {
@@ -20,7 +15,33 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) return "rock";
+    if (randomNumber === 1) return "paper";
+    else return "scissors"
+}
+
+  function playGame(humanChoice) {
+
+        const humanSelection = humanChoice;
+        const computerSelection = getComputerChoice();
+        
+        alert(playRound(humanSelection, computerSelection));
+        alert(`Computer: ${computerScore}\nYou: ${humanScore}`)
+
+    if (humanScore === computerScore) {alert(`Final score: ${computerScore}: ${humanScore}\nA bore draw.`)};
+    if (humanScore > computerScore) {alert(`Final score: ${humanScore}: ${computerScore}\nYou are victorious!`)};
+    if (humanScore < computerScore) {alert(`Final score: ${computerScore}: ${humanScore}\nAI wins and you lose.`)};
+
+    blurb.textContent = 'TADAAAA!';
+    result.appendChild(blurb);
+
+  }
+
+
+
+  function playRound(humanChoice, computerChoice) {
     numberOfGames ++;
     alert(`You chose ${humanChoice} and the computer chose ${computerChoice}.`)
     if (humanChoice === computerChoice) {return "It's a draw!"};
@@ -37,17 +58,16 @@ function playRound(humanChoice, computerChoice) {
     }
   }
 
-  function playGame() {
-    while (numberOfGames < 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        
-        alert(playRound(humanSelection, computerSelection));
-        alert(`Computer: ${computerScore}\nYou: ${humanScore}`)
-    }
-    if (humanScore === computerScore) {alert(`Final score: ${computerScore}: ${humanScore}\nA bore draw.`)};
-    if (humanScore > computerScore) {alert(`Final score: ${humanScore}: ${computerScore}\nYou are victorious!`)};
-    if (humanScore < computerScore) {alert(`Final score: ${computerScore}: ${humanScore}\nAI wins and you lose.`)};
-  }
-  
-  playGame();
+
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    let humanChoice = button.id;
+    playGame(humanChoice);
+  });
+});
