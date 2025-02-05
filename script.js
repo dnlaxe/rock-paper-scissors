cb0a06952443642c3deb93fb0fe08d9bb1392e8c
dnlaxe <195729718+dnlaxe@users.noreply.github.com>
2025-02-05 18:27:23 +0900

    Added name option

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const outcome = document.createElement('span');
-const human = document.createElement('span');
-const computer = document.createElement('span');
-
 const result = document.querySelector('.result');
 const humanPoints = document.querySelector('.humanScore');
 const computerPoints = document.querySelector('.computerScore');
+const buttons = document.querySelectorAll("button");
 
 let humanScore = 0;
 let computerScore = 0;
 let numberOfGames = 0;
+
+var userName = prompt("What is your name?");
+humanPoints.textContent = `${userName}: 0`;
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -22,8 +22,9 @@ function getComputerChoice() {
         const humanSelection = humanChoice;
         const computerSelection = getComputerChoice();
         
-        result.textContent = playRound(humanSelection, computerSelection);
-        humanPoints.textContent = `Human: ${humanScore}`;
+        let outcome = playRound(humanSelection, computerSelection);
+        result.textContent = outcome;
+        humanPoints.textContent = `${userName}: ${humanScore}`;
         computerPoints.textContent = `Computer: ${computerScore}`;
 
         if (computerScore === 5) {alert('Game over! You lose!'); location.reload()};
@@ -47,12 +48,7 @@ function getComputerChoice() {
     } 
   }
 
-  // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll("button");
-
-// we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
   button.addEventListener("click", () => {
     let humanChoice = button.id;
     playGame(humanChoice);
